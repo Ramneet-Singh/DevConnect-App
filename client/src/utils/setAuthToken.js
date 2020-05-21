5d42eb4ecd8181ae46606a7cfc098475fa10e554
Ramneet-Singh <ramneet2001@gmail.com>
2020-05-21 11:07:36 +0530

    App completed

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,11 +1,13 @@
-import axios from "axios"
+import api from "./api"
 
-export default (token) => {
+const setAuthToken = (token) => {
 	if (token) {
-		axios.defaults.headers.common["x-auth-token"] = token
+		api.defaults.headers.common["x-auth-token"] = token
 		localStorage.setItem("token", token)
 	} else {
-		delete axios.defaults.headers.common["x-auth-token"]
+		delete api.defaults.headers.common["x-auth-token"]
 		localStorage.removeItem("token")
 	}
 }
+
+export default setAuthToken
